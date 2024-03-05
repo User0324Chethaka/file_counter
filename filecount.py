782a627559028cc388efa0adfdf489eed3df6650
chethaka <csathmal24@gamil.com>
2024-03-05 10:15:32 +0530

    added types

--- filecount.py
+++ filecount.py
@@ -1,20 +1,18 @@
 import os
-import json
 
 def get_files(path, ls):
-    elements = os.scandir(path=path)
-    for element in elements:
+    for element in os.scandir(path=path):
         if element.is_dir():
             get_files(element.path, ls)
         else:
             ls.append(element.name)
 
-file_ls = []
-path_ = input("path: ")
+file_ls: list[os.DirEntry] = []
+path_: str = input("path: ")
 
 get_files(path=path_, ls=file_ls)
 
-file_types = {a.split(".")[-1] for a in file_ls}
+file_types: set = {a.split(".")[-1] for a in file_ls}
 
 print(f'file count: {len(file_ls)}')
 print(f'types of files: {file_types}')
